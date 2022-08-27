31e90de6d7aef589c0ab3c1f2c90c73e9e60e2e3
Christian <3003html@gmail.com>
2022-08-27 00:45:10 -0500

    fibonacciQuick function created

--- main.go
+++ main.go
@@ -4,6 +4,24 @@ func Add(a, b int) int {
 	return a + b
 }
 
+var m = make(map[int]int, 0)
+
+func fibonacciQuick(n int) int {
+	if n < 2 {
+		return n
+	}
+
+	var f int
+	if v, ok := m[n]; ok {
+		f = v
+	} else {
+		f = fibonacciQuick(n-2) + fibonacciQuick(n-1)
+		m[n] = f
+	}
+
+	return f
+}
+
 func fact(n int) int {
 	if n == 0 {
 		return 1
